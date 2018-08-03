bbca2edf1107532ea5cd85232cf2e76b31ff2621
Narendra <narry051@gmail.com>
2018-08-03 14:15:59 +0530

    Initial commit

---
+++ application/modules/home/views/common/nav.php
@@ -0,0 +1,37 @@
+<div class="container mb-2">
+    <ul class="nav justify-content-end nav-top-part bg-dark">
+        <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa fa-user"></i>&nbsp;&nbsp;Register</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa fa-lock"></i>&nbsp;&nbsp;Login</a>
+        </li>
+    </ul>
+    <div class="card text-white brs">
+        <div class="card-body"><i class="fa fa-play-circle-o"></i>&nbsp;MUFTY MEDIA</div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Submit Video</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">User Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
---
+++ application/modules/home/views/home.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php $this->load->view('common/head'); ?>
+        <style>
+            .videos .card-body p {font-size: 13px;}
+            .videos .card {min-height: 300px;}
+        </style>
+    </head>
+    <body>
+        <?php $this->load->view('common/nav'); ?>
+        <div class="container">
+
+            <div id="carouselExampleFade" class="carousel slide carousel-fade mb-2" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleFade" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleFade" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="<?= root ?>assets/img/sliders/s1.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="<?= root ?>assets/img/sliders/s2.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <div class="mb-2 mt-4 videos">
+                <h5 class="p-2 mb-2 border-left-heading">Recent Videos</h5>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/Dize16eWINU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">APNRT JANMABHOOMI MANA VOORU 2018.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/xLfYje3490U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">APNRT CEO Dr Ravikumar Vemuru About The Iconic & Towering APNRT.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/xq2JbZM3Fdo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">CM Chandrababu To Lay Foundation For APNRT Icon Towers On 22nd June.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/Dize16eWINU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">APNRT JANMABHOOMI MANA VOORU 2018.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-2 mt-4 videos">
+                <h5 class="p-2 mb-2 border-left-heading">Popular Videos</h5>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/Dize16eWINU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">APNRT JANMABHOOMI MANA VOORU 2018.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/xLfYje3490U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">APNRT CEO Dr Ravikumar Vemuru About The Iconic & Towering APNRT.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/xq2JbZM3Fdo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">CM Chandrababu To Lay Foundation For APNRT Icon Towers On 22nd June.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/Dize16eWINU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <p class="card-text">APNRT JANMABHOOMI MANA VOORU 2018.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
